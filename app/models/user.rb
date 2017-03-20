f89bc46accf0e315ed5ea3bccd9067d08e92ce9b
wxlong <wxlong1111@163.com>
2017-03-20 22:53:10 +0800

    add post and hook group/user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   has_many :groups
+  has_many :posts
 end
