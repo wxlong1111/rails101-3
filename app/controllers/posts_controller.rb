4e7a088dc5ca3545cfbec040e64c2d800f084660
wxlong <wxlong1111@163.com>
2017-03-22 22:47:37 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,25 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    @post.update(post_params)
+
+    redirect_to posts_path, notice: "Update Success"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
